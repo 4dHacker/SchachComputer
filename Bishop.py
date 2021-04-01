d73b156eff88a9896cbadf86346a745fcec25c4b
4dHacker <aarongsch@icloud.com>
2021-04-01 23:19:36 +0200

    Bishop corrections again:(

--- Bishop.py
+++ Bishop.py
@@ -1,8 +1,21 @@
-class Bishop:
+class Figure:
+    def __repr__(self):
+        return " "
+
+    def get_moves(self, board, x, y):
+        return []
+
+    def __init__(self):
+        self.empty = True
+
+class Bishop(Figure):
     def __repr__(self):
         return "B" if self.white else "b"
-    def __init__(self):
-        self.white
+    def __init__(self, white):
+        Figure.__init__(self)
+
+        self.empty = False
+        self.white = white
 
     def get_moves(self,board,x,y):
         moves=[]
@@ -11,12 +24,18 @@ class Bishop:
             pos_x, pos_y = x, y
             pos_x += dx
             pos_y += dy
-            while board[pos_x,pos_y]==0 and pos_x<8 and pos_y<8:
-                pos_x+=dx
-                pos_y+=dy
+            while 0<pos_x<8 and 0>pos_y>8 and (board[pos_x,pos_y].empty or board[pos_x,pos_y].white != self.white):
                 moves.append((pos_x, pos_y))
+                pos_x += dx
+                pos_y += dy
 
 
 
 
 
+
+
+
+
+
+board.empty and 0<pos_x<8 and 0<pos_y<8
